b851f01aeaebc2154f6455969933da7fedd0f48f
Mudassir Noor <shaikot560@gmail.com>
2022-01-01 19:11:56 -0800

    initial git files and app logo

---
+++ app/src/main/java/com/mudassir/flagquiz/MainActivityFragment.java
@@ -0,0 +1,47 @@
+package com.mudassir.flagquiz;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.mudassir.flagquiz.databinding.FragmentMainBinding;
+
+public class MainActivityFragment extends Fragment {
+
+    private FragmentMainBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentMainBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(MainActivityFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
